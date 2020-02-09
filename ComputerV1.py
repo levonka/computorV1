9eff8077dee24857cc079174f2502dc2157eafc5
Lymond goodbrook Wyl the whittler <lwyl-the@oa-a3.21-school.ru>
2020-02-09 18:23:13 +0300

    add parser for equation from input

--- ComputerV1.py
+++ ComputerV1.py
@@ -1,54 +1,33 @@
 import sys
-from enum import Enum
+from enum import IntEnum
+
+from math_fucntion import my_abs, my_sqrt
+from colors import Colors
+
+precision = 0
+
 
 class InputException(Exception):
-    '''Пользовательский класс исключения.'''
+    """ Custom exception class. """
+
     def __init__(self, equation):
         Exception.__init__(self)
         self.equation = equation
 
-class Degree(Enum):
+
+class Degree(IntEnum):
     Zero = 0
     First = 1
     Second = 2
 
-def my_abs(x):
-    """ Returns the absolute value of number.
+
+def get_degree(a, b, c):
+    """ Returns equation degree.
 
     Parameters:
-        x (int or float): The number which is to be absolute.
-
-    Returns:
-        int or float: The number which became absolute.
-    """
-    return x if x > 0 else -x
-
-def my_sqrt(n, EPS = 1E-10):
-    """ Returns the sqrt value of number. 
-        The Newton method.
-
-    Parameters:
-        n (int or float): The positive number from which the square root will be extracted.
-        EPS (float): The required accuracy of calculations.
-
-    Returns:
-        float: Square root of a number.
-    """
-    x = 1
-    while (1):
-        nx = (x + n / x) / 2
-        if abs(x - nx) < EPS:
-            break
-        x = nx
-    return x
-
-def degree(a, b, c):
-    """ Returns equation degree. 
-
-    Parameters:
-        a (int or float): Parametr before x^2.
-        b (int or float): Parametr before x^1.
-        c (int or float): Parametr before x^0.
+        a (int or float): Parameter before x^2.
+        b (int or float): Parameter before x^1.
+        c (int or float): Parameter before x^0.
 
     Returns:
         int: equation degree.
@@ -60,60 +39,153 @@ def degree(a, b, c):
     else:
         return Degree.Second
 
+
 def discriminant(a, b, c):
     """ Returns the discriminant.
 
     Parameters:
-        a (int or float): Parametr before x^2.
-        b (int or float): Parametr before x^1.
-        c (int or float): Parametr before x^0.
+        a (int or float): Parameter before x^2.
+        b (int or float): Parameter before x^1.
+        c (int or float): Parameter before x^0.
 
     Returns:
         int or float: discriminant.
     """
     return b * b - 4 * a * c
 
-def easy_solv(c):
+
+def easy_solution(c):
     if c == 0:
-        print("|R")
+        print("|R (all the real numbers are solution)")
     else:
         print("The equation has no solution")
     return
 
-def linear_equation_solv(b, c):
-    print("The solution is: " -c / b)
+
+def linear_equation_solution(b, c):
+    print("The solution is: ".format(round(- c / b), precision))
     return
 
-def second_degree_equation_solv(a, b, c):
+
+def second_degree_equation_solution(a, b, c):
     D = discriminant(a, b, c)
     if D != 0:
         sqrt = my_sqrt(my_abs(D))
-        first_solv = (-b + sqrt) / (2 * a)
-        second_solv = (-b - sqrt) / (2 * a)
+        first_part = -b / (2 * a)
+        second_part = sqrt / (2 * a)
         if D > 0:
             print("Discriminant greater than zero")
-            print("First solution is: ", format(first_solv, '.2f'))
-            print("Second solution is: ", format(second_solv, '.2f'))
+            print("First solution is: {0}"
+                  .format(round(first_part + second_part, precision)))
+            print("Second solution is: {0}"
+                  .format(round(first_part - second_part, precision)))
         else:
             print("Discriminant less than zero")
             print("We have complex solution.")
-            print("First solution is: ", format(first_solv, '.2f'))
-            print("Second solution is: ", format(second_solv, '.2f'))
+            print("First solution is: {0} + {1} * i"
+                  .format(round(first_part, precision), round(second_part, precision)))
+            print("Second solution is: {0} - {1} * i"
+                  .format(round(first_part, precision), round(second_part, precision)))
     else:
         print("Discriminant is zero")
         print("We have one double-solution.")
-        print("Solution is: ", format(-b / (2 * a), '.2f'))
+        print("Solution is: {0}"
+              .format(round(-b / (2 * a), precision)))
 
     return
 
-def test(x):
-    if x != 'X':
-        raise InputException(x)
-    return
 
-if __name__== "__main__":
+def reduced_form(a, b, c):
+    print("Reduced form: {0} * X^2 + {1} * X^1 + {2} * X^0 = 0\n"
+          .format(a, b, c))
+
+
+def print_info_about_equation(a, b, c, power):
+    print("\nPolynomial degree: {}\n".format(power))
+    reduced_form(a, b, c)
+
+
+def get_coefficients(part):
+    a = 0.0
+    b = 0.0
+    c = 0.0
+
+    for elem in part:
+        if elem == '0' or elem == '':
+            continue
+
+        coeff, var = elem.split('*')
+        var, power = var.split('^')
+
+        if var != 'X' and var != 'x':
+            raise InputException('The variable can only be X or x')
+
+        try:
+            if int(power) == Degree.Second:
+                a += float(coeff)
+            elif int(power) == Degree.First:
+                b += float(coeff)
+            elif int(power) == Degree.Zero:
+                c += float(coeff)
+            else:
+                print("Polynomial degree: {0}".format(int(power)))
+                sys.exit("The polynomial degree is strictly greater than 2, I can't solve.")
+
+        except ValueError:
+            raise InputException('Incorrect power. Must be greater then 0')
+
+    return a, b, c
+
+
+def solve(equation):
+    a = 0
+    b = 0
+    c = 0
+
+    equation = equation.replace(' ', '').replace('-', '+-').split('=')
+
+    parts = [equation[i].split('+') for i in range(len(equation))]
+
+    if len(parts) != 2:
+        raise InputException("Wrong input. An equation has more than one equal sign or none at all")
+
+    for index, part in enumerate(parts):
+        a_tmp, b_tmp, c_tmp = get_coefficients(part)
+
+        if index == 0:
+            a += a_tmp
+            b += b_tmp
+            c += c_tmp
+        else:
+            a -= a_tmp
+            b -= b_tmp
+            c -= c_tmp
+
+    degree = get_degree(a, b, c)
+
+    print_info_about_equation(a, b, c, degree)
+
+    if degree == Degree.Second:
+        second_degree_equation_solution(a, b, c)
+    elif degree == Degree.First:
+        linear_equation_solution(b, c)
+    else:
+        easy_solution(c)
+
+    return a, b, c
+
+
+if __name__ == "__main__":
+    print("Equation format: A*X^2+B*X^1+C*X^0=D*X^0.\nQuadratic polynomials or simpler.")
+    equation = input("Input equation: ")
+
     try:
-        print(test('lol'))
+        precision = int(input("Input precision (default is 2): "))
+    except ValueError:
+        precision = 2
+
+    try:
+        solve(equation)
     except InputException as ex:
-        print('InputException: Уравнение -- {0} введено не корректно;\nОжидалось, например: a*X^2+b*X^1+c*X^0=0'.format(ex.equation))
-    #print(my_abs.__doc__ )
+        print(f"{Colors.FAIL}\nInputException: Equation -- entered incorrectly.\nProblem: {ex.equation};\n"
+              "Expected, for example: A*X^2+B*X^1+C*X^0=D*X^0")
